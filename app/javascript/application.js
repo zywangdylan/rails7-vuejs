124a8dbb0b954c8292a6c56dc5104de2140a2dca
dylan wang <zywangdylan@gmail.com>
2022-04-13 16:22:59 +0800

    init rails 7 with vue

---
+++ app/javascript/application.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+// import Vue from '/vue/dist/vue.esm'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   var app = new Vue({
+//     el: '#app',
+//     data: {
+//       message: 'Hello Vue!'
+//     }
+//   })
+// })
